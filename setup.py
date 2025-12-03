1e05bd7604d22b539588e003900db847c128d8c3
a-krawciw <freakyfactoid@gmail.com>
2025-12-02 19:33:43 -0500

    Add config folder to install

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
 	(os.path.join('share', package_name, 'launch'),
         glob(os.path.join('launch', '*launch.[pxy][yma]*'))), # This line adds the launch files
+        (os.path.join('share', package_name, 'config'),
+        glob(os.path.join('config', '*.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
